8690c5e34861ec11870e07c88fe4b36386424233
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-25 15:49:04 +0100

    Snack 1 DONE

--- snack1/index.php
+++ snack1/index.php
@@ -5,10 +5,19 @@ Snack 1
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 */
 
-$name=$_GET['name'];
-$mail=$_GET['mail'];
-$age=$_GET['age'];
+$name = $_GET['name'] ?? '';
+$mail = $_GET['mail'] ?? '';
+$age = $_GET['age'] ?? '';
 
+$access = '';
+
+if (strlen($name) >= 2 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
+   $access = 'Accesso riuscito';
+   $color = 'text-info';
+} else {
+   $access = 'Accesso negato';
+   $color = 'text-danger';
+}
 
 ?>
 
@@ -28,12 +37,15 @@ $age=$_GET['age'];
          <h1>Raccolta dati</h1>
          <label for="name">Inserisci il tuo nome</label>
          <input id="name" type="text" name="name">
-         <label for="mail">Inserisci il tuo cognome</label>
+         <label for="mail">Inserisci la tua mail</label>
          <input id="mail" type="mail" name="mail">
          <label for="age">Inserisci la tua età</label>
          <input id="age" type="number" name="age" max="120">
          <button type="submit">INVIA</button>
       </form>
+      <h2 class="<?php echo $color ?> my-4">
+         <?php echo $access ?>
+      </h2>
    </div>
 </body>
 
